3caf32c172968d02a58d058f9b25e74ef546bb71
jmgisele <james.may.gisele@protonmail.com>
2022-01-08 19:19:00 -0500

    final project

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/cardlist/card-list.component.jsx'
+import { SearchBox } from './components/search/search-component.jsx'
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
+
   }
+
+  updateState = (values) => {
+    this.setState(values);
+  }
+
+  async componentDidMount() {
+    const users = await (await fetch('https://jsonplaceholder.typicode.com/users')).json();
+    this.setState({ monsters: users });
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
     return (
       <div className='App'>
-        <CardList>
-          {
-            this.state.monsters.map(monster => <h2 key={monster.id}>{monster.name}</h2>)
-          }
+        <h1> Monsters Rolodex </h1>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}>
         </CardList>
-
       </div>
     )
-  }
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(users => this.setState({ monsters: users }))
   }
 }
 
--- src/components/cardlist/card-list.component.jsx
+++ src/components/cardlist/card-list.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './card-list.styles.css';
 
-export const CardList = (props) => {
-    console.log(props)
+import { Card } from '../card/card-component';
+
+export const CardList = props => {
     return (
-        <div className='card-list'>{props.children}</div>
+        <div className='card-list'>
+            {props.monsters.map(monster => (
+                <Card key={monster.id} monster={monster} />
+            ))}
+        </div>
     )
-}
+} 
